ce34cc54d31d1c4041855fa9fdcf625084b71fd8
jackyfialho <teixeijf@gmail.com>
2020-06-19 18:11:30 -0300

    aplicação pronta sem validações

---
+++ mobile/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { AppLoading } from 'expo';
+import { StatusBar } from 'react-native';
+
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'; //precisa importar o useFontes em qlqr uma das duas
+
+import Routes from './src/routes';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    // fragment - tag q não produz conteúdo
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Routes />
+    </>
+  );
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,28 @@
-import express, { RouterOptions } from 'express';
+import express from 'express';
+
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 import TesteController from './controllers/TesteController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 const testeController = new TesteController();
+
+routes.get('/items', itemsController.index);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+routes.post('/teste', testeController.create);
+
+// caso receba mais d uma image, usar array em vez de single
+routes.post('/points', upload.single('image'), pointsController.create);
+
+export default routes;
 
 // const users = [
 //     'Diego',
@@ -25,12 +40,3 @@ const testeController = new TesteController();
 // });
 
 // index, show (trazer específico), create, update, delete
-
-routes.get('/items', itemsController.index);
-
-routes.post('/points', pointsController.create);
-routes.get('/points', pointsController.index);
-routes.get('/points/:id', pointsController.show);
-
-routes.post('/teste', testeController.create);
-export default routes;
